28d2b71885aec60fc24fd5d45292cca9e695be83
Asiphe Gwala <asiphe4g@gmail.com>
2026-08-24 14:58:04 +0200

    Complete Q5.2 level progression

--- POE_Rich_Mtk_Asiphe_Louw/GameEngine.cs
+++ POE_Rich_Mtk_Asiphe_Louw/GameEngine.cs
@@ -9,11 +9,14 @@ namespace POE_Rich_Mtk_Asiphe_Louw
 
         private Level currentLevel;
         private int numberOfLevels;
+        private int currentLevelNumber;
         private Random random;
+        private GameState gameState = GameState.InProgress;
 
         public GameEngine(int numberOfLevels)
         {
             this.numberOfLevels = numberOfLevels;
+            currentLevelNumber = 1;
             random = new Random();
 
             int width = random.Next(MIN_SIZE, MAX_SIZE + 1);
@@ -31,6 +34,18 @@ namespace POE_Rich_Mtk_Asiphe_Louw
             HeroTile hero = currentLevel.Hero;
             Tile targetTile = hero.Vision[(int)direction];
 
+            if (targetTile is ExitTile)
+            {
+                if (currentLevelNumber == numberOfLevels)
+                {
+                    gameState = GameState.Complete;
+                    return false;
+                }
+
+                NextLevel();
+                return true;
+            }
+
             if (!(targetTile is EmptyTile))
             {
                 return false;
@@ -41,6 +56,16 @@ namespace POE_Rich_Mtk_Asiphe_Louw
             return true;
         }
 
+        private void NextLevel()
+        {
+            currentLevelNumber++;
+            HeroTile hero = currentLevel.Hero;
+
+            int width = random.Next(MIN_SIZE, MAX_SIZE + 1);
+            int height = random.Next(MIN_SIZE, MAX_SIZE + 1);
+            currentLevel = new Level(width, height, hero);
+        }
+
         public void TriggerMovement(Direction direction)
         {
             MoveHero(direction);
@@ -48,6 +73,11 @@ namespace POE_Rich_Mtk_Asiphe_Louw
 
         public override string ToString()
         {
+            if (gameState == GameState.Complete)
+            {
+                return "Congratulations! You have successfully completed the game.";
+            }
+
             return currentLevel.ToString();
         }
     }
---
+++ POE_Rich_Mtk_Asiphe_Louw/GameState.cs
@@ -0,0 +1,9 @@
+namespace POE_Rich_Mtk_Asiphe_Louw
+{
+    internal enum GameState
+    {
+        InProgress,
+        Complete,
+        GameOver
+    }
+}
